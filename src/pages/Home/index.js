adb4bda33889c9badb210e7f4115be779210750d
Raul Carlos <raulcarlos96@hotmail.com>
2024-02-05 22:21:33 -0300

    pagina de login

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,51 @@
-
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import './home.css'
 
 export default function Home(){
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleLogin(e){
+    e.preventDefault();
+
+    if(email !== '' && password !== ''){
+      alert("teste")
+    }else{
+      alert("Preencha todos os campos!")
+    }
+  }
+
+
+
     return(
-      <div>
-        <h1>pagina home</h1>
+      <div className="home-container">
+        <h1>Lista de tarefas</h1>
+        <span>Gerencie sua agenda de forma facil.</span>
+
+        <form className="form" onSubmit={handleLogin}>
+          <input 
+            type="text"
+            placeholder="Digite seu email..."
+            value={email}
+            onChange={(e)=> setEmail(e.target.value)}
+          />
+          <input 
+            autoComplete={false}
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={(e)=> setPassword(e.target.value)}
+          />
+
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link className="button-link" to="/register">
+          Não possui uma conta? Cadastre-se
+        </Link>
+
       </div>
     )
   }
