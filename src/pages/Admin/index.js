e2f531c1b78fa758a576316b75d1f9e16d08a0af
Raul Carlos <raulcarlos96@hotmail.com>
2024-02-08 21:44:03 -0300

    pagina de admin

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,9 +1,45 @@
+import { useState } from 'react'
 import './admin.css'
+import { auth } from '../../firebaseConnection'
+import { signOut } from 'firebase/auth'
 
 export default function Admin(){
+
+    const [tarefaInput, setTarefaInput] = useState('')
+
+    function handleRegister(e){
+        e.preventDefault();
+    }
+
+    async function handleLogout(){
+        await signOut(auth);
+    }
+
     return(
-        <div>
-            <h1>pagina admin</h1>
+        <div className='admin-container'>
+            <h1>Minhas tarefas</h1>
+
+            <form className='form' onSubmit={handleRegister}>
+                <textarea 
+                    placeholder='Digite sua tarefa...'
+                    value={tarefaInput}
+                    onChange={ (e) => setTarefaInput(e.target.value) }
+                />
+
+                <button className='btn-register' type='submit'>Registrar tarefa</button>      
+            </form>
+
+            <article className='list'>
+                <p>Estudar JS</p>
+
+                <div>
+                    <button>Editar</button>
+                    <button className='btn-delete'>Concluir</button>
+                </div>
+            </article>
+
+            <button className='btn-logout' onClick={handleLogout}>Sair</button>
+
         </div>
     )
 }
